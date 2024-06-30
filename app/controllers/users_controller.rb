e263571a7326793b402a59b3d082fbb963c92c7b
Kurt Mauritz <38671493+MauritzKurt@users.noreply.github.com>
2024-06-30 22:25:46 +0000

    User details works correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
     matching_users = User.all
 
     @list_of_users = matching_users.order({ :username => :asc })
-    
+
     render({ :template => "/user_templates/index" })
+  end
+
+  def show
+    # Parameters: {"path_username"=>"catherina"}
+
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.at(0)
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "/user_templates/show" })
+    end
   end
 end
