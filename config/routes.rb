26c6ad2ab34abce97ba1dee4b45e083fe879763c
Kurt Mauritz <38671493+MauritzKurt@users.noreply.github.com>
2024-07-01 04:13:41 +0000

    photo create works

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_photoid", { :controller => "photos", :action => "show" })
+  get("/insert_photo", { :controller => "photos", :action => "create" })
   get("/delete_photo/:path_id", { :controller => "photos", :action => "delete" })
 end
